5fca98e10dceeee039ee332197ea485a1ed53725
renukumar07 <renukumar90@gmail.com>
2022-10-25 15:16:06 +0530

    19. Code Demo - Rough Prototype Part 1

---
+++ spring-demo-one/src/com/renu/springdemo/MyApp.java
@@ -0,0 +1,13 @@
+package com.renu.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		//Create the object
+		Coach theCoach = new BaseballCoach();
+		//Use the object
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
