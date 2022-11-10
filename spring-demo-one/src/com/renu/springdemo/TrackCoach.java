7078ba1bc012a63804fcb449c09d0cc1f024712f
renukumar07 <renukumar90@gmail.com>
2022-11-10 22:42:13 +0530

    32. Spring Dependency Injection - Part 3 - Track Coach

--- spring-demo-one/src/com/renu/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/renu/springdemo/TrackCoach.java
@@ -2,6 +2,15 @@ package com.renu.springdemo;
 
 public class TrackCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {
+		
+	}
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Run a hard 5k";
@@ -9,8 +18,7 @@ public class TrackCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Just Do It: "+fortuneService.getFortune();
 	}
 
 }
