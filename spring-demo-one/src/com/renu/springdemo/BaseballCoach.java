6b3f7ea7202687004bd05bbb92c428ee5f112cec
renukumar07 <renukumar90@gmail.com>
2022-11-10 22:29:24 +0530

    31. Spring Dependency Injection - Part 2

--- spring-demo-one/src/com/renu/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/renu/springdemo/BaseballCoach.java
@@ -2,6 +2,13 @@ package com.renu.springdemo;
 
 public class BaseballCoach implements Coach{
 	
+	// define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	//define a constructor for dependency injection
+	private BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
@@ -9,8 +16,9 @@ public class BaseballCoach implements Coach{
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		//use my fortuneService to get a fortune
+		return fortuneService.getFortune();
 	}
 
 }
